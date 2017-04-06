6f5645400564cbeaaa0949d24af1e283aaa03813
kyle <kyleflemington@gmail.com>
2017-04-05 18:27:37 -0700

    Console is error free | not rendering

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ var path		= require('path');
 module.exports = {
 	devtool: 'inline-source-map',
 	entry: [
-		'webpack-dev-server/client?http://127.0.0.1.8080',
 		'webpack/hot/only-dev-server',
 		'./src/components/app.js'
 	],
