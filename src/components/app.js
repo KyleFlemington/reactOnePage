3c371267e9873bef792c345d2bbe2e2009046e91
kyle <kyleflemington@gmail.com>
2017-04-05 17:47:54 -0700

    error fixed - capitalization -- not rendering

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-export default class App extends React.component {
+export default class App extends React.Component {
 	render() {
 		return (
-			<div>
-				<h1> React To Dos App </h1>
-			</div>
+				<div>
+					<h1> React To Dos App </h1>
+				</div>
 		);
 	}
 }
